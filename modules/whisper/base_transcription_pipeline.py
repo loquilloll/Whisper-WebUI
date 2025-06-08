5af8a890f6b2a93d3fdb0c85d6d1688bd5ca32f1
Alvin Sanchez <asanchez@korewireless.com>
2025-06-08 15:41:48 -0400

    fix: improve code formatting and consistency in base_transcription_pipeline.py

--- modules/whisper/base_transcription_pipeline.py
+++ modules/whisper/base_transcription_pipeline.py
@@ -13,8 +13,13 @@ from copy import deepcopy
 import time
 
 from modules.uvr.music_separator import MusicSeparator
-from modules.utils.paths import (WHISPER_MODELS_DIR, DIARIZATION_MODELS_DIR, OUTPUT_DIR, DEFAULT_PARAMETERS_CONFIG_PATH,
-                                 UVR_MODELS_DIR)
+from modules.utils.paths import (
+    WHISPER_MODELS_DIR,
+    DIARIZATION_MODELS_DIR,
+    OUTPUT_DIR,
+    DEFAULT_PARAMETERS_CONFIG_PATH,
+    UVR_MODELS_DIR,
+)
 from modules.utils.constants import *
 from modules.utils.logger import get_logger
 from modules.utils.subtitle_manager import *
@@ -30,24 +35,20 @@ logger = get_logger()
 
 
 class BaseTranscriptionPipeline(ABC):
-    def __init__(self,
-                 model_dir: str = WHISPER_MODELS_DIR,
-                 diarization_model_dir: str = DIARIZATION_MODELS_DIR,
-                 uvr_model_dir: str = UVR_MODELS_DIR,
-                 output_dir: str = OUTPUT_DIR,
-                 ):
+    def __init__(
+        self,
+        model_dir: str = WHISPER_MODELS_DIR,
+        diarization_model_dir: str = DIARIZATION_MODELS_DIR,
+        uvr_model_dir: str = UVR_MODELS_DIR,
+        output_dir: str = OUTPUT_DIR,
+    ):
         self.model_dir = model_dir
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
         os.makedirs(self.model_dir, exist_ok=True)
-        self.diarizer = Diarizer(
-            model_dir=diarization_model_dir
-        )
+        self.diarizer = Diarizer(model_dir=diarization_model_dir)
         self.vad = SileroVAD()
-        self.music_separator = MusicSeparator(
-            model_dir=uvr_model_dir,
-            output_dir=os.path.join(output_dir, "UVR")
-        )
+        self.music_separator = MusicSeparator(model_dir=uvr_model_dir, output_dir=os.path.join(output_dir, "UVR"))
 
         self.model = None
         self.current_model_size = None
@@ -58,32 +59,30 @@ class BaseTranscriptionPipeline(ABC):
         self.current_compute_type = self.get_compute_type()
 
     @abstractmethod
-    def transcribe(self,
-                   audio: Union[str, BinaryIO, np.ndarray],
-                   progress: gr.Progress = gr.Progress(),
-                   progress_callback: Optional[Callable] = None,
-                   *whisper_params,
-                   ):
+    def transcribe(
+        self,
+        audio: Union[str, BinaryIO, np.ndarray],
+        progress: gr.Progress = gr.Progress(),
+        progress_callback: Optional[Callable] = None,
+        *whisper_params,
+    ):
         """Inference whisper model to transcribe"""
         pass
 
     @abstractmethod
-    def update_model(self,
-                     model_size: str,
-                     compute_type: str,
-                     progress: gr.Progress = gr.Progress()
-                     ):
+    def update_model(self, model_size: str, compute_type: str, progress: gr.Progress = gr.Progress()):
         """Initialize whisper model"""
         pass
 
-    def run(self,
-            audio: Union[str, BinaryIO, np.ndarray],
-            progress: Optional[gr.Progress] = None,
-            file_format: str = "SRT",
-            add_timestamp: bool = True,
-            progress_callback: Optional[Callable] = None,
-            *pipeline_params,
-            ) -> Tuple[List[Segment], float]:
+    def run(
+        self,
+        audio: Union[str, BinaryIO, np.ndarray],
+        progress: Optional[gr.Progress] = None,
+        file_format: str = "SRT",
+        add_timestamp: bool = True,
+        progress_callback: Optional[Callable] = None,
+        *pipeline_params,
+    ) -> Tuple[List[Segment], float]:
         """
         Run transcription with conditional pre-processing and post-processing.
         The VAD will be performed to remove noise from the audio input in pre-processing, if enabled.
@@ -122,7 +121,12 @@ class BaseTranscriptionPipeline(ABC):
 
         params = TranscriptionPipelineParams.from_list(list(pipeline_params))
         params = self.validate_gradio_values(params)
-        bgm_params, vad_params, whisper_params, diarization_params = params.bgm_separation, params.vad, params.whisper, params.diarization
+        bgm_params, vad_params, whisper_params, diarization_params = (
+            params.bgm_separation,
+            params.vad,
+            params.whisper,
+            params.diarization,
+        )
 
         if bgm_params.is_separate_bgm:
             music, audio, _ = self.music_separator.separate(
@@ -131,7 +135,7 @@ class BaseTranscriptionPipeline(ABC):
                 device=bgm_params.uvr_device,
                 segment_size=bgm_params.segment_size,
                 save_file=bgm_params.save_file,
-                progress=progress
+                progress=progress,
             )
 
             if audio.ndim >= 2:
@@ -156,14 +160,10 @@ class BaseTranscriptionPipeline(ABC):
                 min_speech_duration_ms=vad_params.min_speech_duration_ms,
                 max_speech_duration_s=vad_params.max_speech_duration_s,
                 min_silence_duration_ms=vad_params.min_silence_duration_ms,
-                speech_pad_ms=vad_params.speech_pad_ms
+                speech_pad_ms=vad_params.speech_pad_ms,
             )
 
-            vad_processed, speech_chunks = self.vad.run(
-                audio=audio,
-                vad_parameters=vad_options,
-                progress=progress
-            )
+            vad_processed, speech_chunks = self.vad.run(audio=audio, vad_parameters=vad_options, progress=progress)
 
             if vad_processed.size > 0:
                 audio = vad_processed
@@ -171,10 +171,7 @@ class BaseTranscriptionPipeline(ABC):
                 vad_params.vad_filter = False
 
         result, elapsed_time_transcription = self.transcribe(
-            audio,
-            progress,
-            progress_callback,
-            *whisper_params.to_list()
+            audio, progress, progress_callback, *whisper_params.to_list()
         )
         if whisper_params.enable_offload:
             self.offload()
@@ -194,18 +191,16 @@ class BaseTranscriptionPipeline(ABC):
                 progress(0.99, desc="Diarizing speakers..")
             result, elapsed_time_diarization = self.diarizer.run(
                 audio=origin_audio,
-                use_auth_token=diarization_params.hf_token if diarization_params.hf_token else os.environ.get("HF_TOKEN"),
+                use_auth_token=(
+                    diarization_params.hf_token if diarization_params.hf_token else os.environ.get("HF_TOKEN")
+                ),
                 transcribed_result=result,
-                device=diarization_params.diarization_device
+                device=diarization_params.diarization_device,
             )
             if diarization_params.enable_offload:
                 self.diarizer.offload()
 
-        self.cache_parameters(
-            params=params,
-            file_format=file_format,
-            add_timestamp=add_timestamp
-        )
+        self.cache_parameters(params=params, file_format=file_format, add_timestamp=add_timestamp)
 
         if not result:
             logger.info(f"Whisper did not detected any speech segments in the audio.")
@@ -216,16 +211,17 @@ class BaseTranscriptionPipeline(ABC):
         total_elapsed_time = time.time() - start_time
         return result, total_elapsed_time
 
-    def transcribe_file(self,
-                        files: Optional[List] = None,
-                        input_folder_path: Optional[str] = None,
-                        include_subdirectory: Optional[str] = None,
-                        save_same_dir: Optional[str] = None,
-                        file_format: str = "SRT",
-                        add_timestamp: bool = True,
-                        progress=gr.Progress(),
-                        *pipeline_params,
-                        ) -> Tuple[str, List]:
+    def transcribe_file(
+        self,
+        files: Optional[List] = None,
+        input_folder_path: Optional[str] = None,
+        include_subdirectory: Optional[str] = None,
+        save_same_dir: Optional[str] = None,
+        file_format: str = "SRT",
+        add_timestamp: bool = True,
+        progress=gr.Progress(),
+        *pipeline_params,
+    ) -> Tuple[str, List]:
         """
         Write subtitle file from Files
         ...
@@ -233,9 +229,7 @@ class BaseTranscriptionPipeline(ABC):
         # unchanged
         try:
             params = TranscriptionPipelineParams.from_list(list(pipeline_params))
-            writer_options = {
-                "highlight_words": True if params.whisper.word_timestamps else False
-            }
+            writer_options = {"highlight_words": True if params.whisper.word_timestamps else False}
 
             if input_folder_path:
                 files = get_media_files(input_folder_path, include_sub_directory=include_subdirectory)
@@ -264,7 +258,7 @@ class BaseTranscriptionPipeline(ABC):
                         output_format=file_format,
                         result=transcribed_segments,
                         add_timestamp=add_timestamp,
-                        **writer_options
+                        **writer_options,
                     )
 
                 subtitle, file_path = generate_file(
@@ -273,39 +267,42 @@ class BaseTranscriptionPipeline(ABC):
                     output_format=file_format,
                     result=transcribed_segments,
                     add_timestamp=add_timestamp,
-                    **writer_options
+                    **writer_options,
                 )
-                files_info[file_name] = {"subtitle": read_file(file_path), "time_for_task": time_for_task, "path": file_path}
+                files_info[file_name] = {
+                    "subtitle": read_file(file_path),
+                    "time_for_task": time_for_task,
+                    "path": file_path,
+                }
 
-            total_result = ''
+            total_result = ""
             total_time = 0
             for file_name, info in files_info.items():
-                total_result += '------------------------------------\n'
-                total_result += f'{file_name}\n\n'
+                total_result += "------------------------------------\n"
+                total_result += f"{file_name}\n\n"
                 total_result += f'{info["subtitle"]}'
                 total_time += info["time_for_task"]
 
             result_str = f"Done in {self.format_time(total_time)}! Subtitle is in the outputs folder.\n\n{total_result}"
-            result_file_path = [info['path'] for info in files_info.values()]
+            result_file_path = [info["path"] for info in files_info.values()]
 
             return result_str, result_file_path
 
         except Exception as e:
             raise RuntimeError(f"Error transcribing file: {e}") from e
 
-    def transcribe_mic(self,
-                       mic_audio: str,
-                       file_format: str = "SRT",
-                       add_timestamp: bool = True,
-                       progress=gr.Progress(),
-                       *pipeline_params,
-                       ) -> Tuple[str, str]:
+    def transcribe_mic(
+        self,
+        mic_audio: str,
+        file_format: str = "SRT",
+        add_timestamp: bool = True,
+        progress=gr.Progress(),
+        *pipeline_params,
+    ) -> Tuple[str, str]:
         # unchanged
         try:
             params = TranscriptionPipelineParams.from_list(list(pipeline_params))
-            writer_options = {
-                "highlight_words": True if params.whisper.word_timestamps else False
-            }
+            writer_options = {"highlight_words": True if params.whisper.word_timestamps else False}
 
             progress(0, desc="Loading Audio..")
             transcribed_segments, time_for_task = self.run(
@@ -325,27 +322,28 @@ class BaseTranscriptionPipeline(ABC):
                 output_format=file_format,
                 result=transcribed_segments,
                 add_timestamp=add_timestamp,
-                **writer_options
+                **writer_options,
             )
 
-            result_str = f"Done in {self.format_time(time_for_task)}! Subtitle file is in the outputs folder.\n\n{subtitle}"
+            result_str = (
+                f"Done in {self.format_time(time_for_task)}! Subtitle file is in the outputs folder.\n\n{subtitle}"
+            )
             return result_str, file_path
         except Exception as e:
             raise RuntimeError(f"Error transcribing mic: {e}") from e
 
-    def transcribe_youtube(self,
-                           youtube_link: str,
-                           file_format: str = "SRT",
-                           add_timestamp: bool = True,
-                           progress=gr.Progress(),
-                           *pipeline_params,
-                           ) -> Tuple[str, str]:
+    def transcribe_youtube(
+        self,
+        youtube_link: str,
+        file_format: str = "SRT",
+        add_timestamp: bool = True,
+        progress=gr.Progress(),
+        *pipeline_params,
+    ) -> Tuple[str, str]:
         # unchanged
         try:
             params = TranscriptionPipelineParams.from_list(list(pipeline_params))
-            writer_options = {
-                "highlight_words": True if params.whisper.word_timestamps else False
-            }
+            writer_options = {"highlight_words": True if params.whisper.word_timestamps else False}
 
             progress(0, desc="Loading Audio from Youtube..")
             yt = get_ytdata(youtube_link)
@@ -369,10 +367,12 @@ class BaseTranscriptionPipeline(ABC):
                 output_format=file_format,
                 result=transcribed_segments,
                 add_timestamp=add_timestamp,
-                **writer_options
+                **writer_options,
             )
 
-            result_str = f"Done in {self.format_time(time_for_task)}! Subtitle file is in the outputs folder.\n\n{subtitle}"
+            result_str = (
+                f"Done in {self.format_time(time_for_task)}! Subtitle file is in the outputs folder.\n\n{subtitle}"
+            )
 
             if os.path.exists(audio):
                 os.remove(audio)
@@ -459,10 +459,7 @@ class BaseTranscriptionPipeline(ABC):
         try:
             device = torch.device("mps")
             sparse_tensor = torch.sparse_coo_tensor(
-                indices=torch.tensor([[0, 1], [2, 3]]),
-                values=torch.tensor([1, 2]),
-                size=(4, 4),
-                device=device
+                indices=torch.tensor([[0, 1], [2, 3]]), values=torch.tensor([1, 2]), size=(4, 4), device=device
             )
             return True
         except RuntimeError:
@@ -505,15 +502,11 @@ class BaseTranscriptionPipeline(ABC):
         if params.whisper.language_detection_threshold == GRADIO_NONE_NUMBER_MIN:
             params.whisper.language_detection_threshold = None
         if params.vad.max_speech_duration_s == GRADIO_NONE_NUMBER_MAX:
-            params.vad.max_speech_duration_s = float('inf')
+            params.vad.max_speech_duration_s = float("inf")
         return params
 
     @staticmethod
-    def cache_parameters(
-        params: TranscriptionPipelineParams,
-        file_format: str = "SRT",
-        add_timestamp: bool = True
-    ):
+    def cache_parameters(params: TranscriptionPipelineParams, file_format: str = "SRT", add_timestamp: bool = True):
         """Cache parameters to the yaml file"""
         cached_params = load_yaml(DEFAULT_PARAMETERS_CONFIG_PATH)
         param_to_cache = params.to_dict()
@@ -532,23 +525,26 @@ class BaseTranscriptionPipeline(ABC):
             language_dict = whisper.tokenizer.LANGUAGES
             cached_yaml["whisper"]["lang"] = language_dict[cached_yaml["whisper"]["lang"]]
 
-        if cached_yaml["vad"].get("max_speech_duration_s", float('inf')) == float('inf'):
+        if cached_yaml["vad"].get("max_speech_duration_s", float("inf")) == float("inf"):
             cached_yaml["vad"]["max_speech_duration_s"] = GRADIO_NONE_NUMBER_MAX
 
         if cached_yaml is not None and cached_yaml:
             save_yaml(cached_yaml, DEFAULT_PARAMETERS_CONFIG_PATH)
 
     @staticmethod
-    def resample_audio(audio: Union[str, np.ndarray],
-                       new_sample_rate: int = 16000,
-                       original_sample_rate: Optional[int] = None,) -> np.ndarray:
+    def resample_audio(
+        audio: Union[str, np.ndarray],
+        new_sample_rate: int = 16000,
+        original_sample_rate: Optional[int] = None,
+    ) -> np.ndarray:
         """Resamples audio to 16k sample rate, standard on Whisper model"""
         if isinstance(audio, str):
             audio, original_sample_rate = torchaudio.load(audio)
         else:
             if original_sample_rate is None:
                 raise ValueError("original_sample_rate must be provided when audio is numpy array.")
-            audio = torch.from_numpy(audio)
+            # Ensure numpy array is writable for PyTorch
+            audio = torch.from_numpy(np.array(audio, copy=True))
         resampler = torchaudio.transforms.Resample(orig_freq=original_sample_rate, new_freq=new_sample_rate)
         resampled_audio = resampler(audio).numpy()
         return resampled_audio
